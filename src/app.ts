06133b6f2aee4e89d75a950dab195beab133ce0b
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-06-10 22:06:48 -0300

    chore: setup node and ts environment and add base files

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+// import cors from 'cors';
+import dotenv from 'dotenv';
+import express, { Response, Request, NextFunction } from 'express';
+import 'express-async-errors';
+
+dotenv.config();
+
+const app = express();
+
+// app.use(cors());
+app.use(express.json());
+
+// app.use(router);
+app.get('/', (req, res) => {
+  return res.json('OK');
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  // if (err instanceof AppError) {
+  //   return response.status(err.statusCode).json({
+  //     message: err.message,
+  //   });
+  // }
+
+  return response.status(500).json({
+    message: `Internal server error - ${err.message}`,
+  });
+});
+
+export { app };
